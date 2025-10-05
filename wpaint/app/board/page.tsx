eb3fd09bfb1936536bf59dfe8894764e6086ba86
AJ-SM <anujsidam1108@gmail.com>
2025-10-05 21:37:54 +0530

    added pointer

--- wpaint/app/board/page.tsx
+++ wpaint/app/board/page.tsx
@@ -1,31 +1,67 @@
 
 "use client"
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 
 export default function Board(){
     const canvaref = useRef<HTMLCanvasElement>(null)
+    const pointer = useRef({x:0,y:0})
+
+    function updatepointer(e:MouseEvent){
+        pointer.current = {x:e.clientX,y:e.clientY}
+    }
+
+
 
     useEffect(()=>{
+        window.addEventListener('mousemove',(e)=>updatepointer(e))
         const canva = canvaref.current
         if(!canva) return;
 
         const ctx = canva.getContext('2d');
         if(!ctx) return;
-        ctx.beginPath();
-        ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-        ctx.fillStyle = "white";
-        ctx.fill()
+
+            const draw = ()=>{
+            ctx.fillStyle = "black";
+            ctx.fillRect(0, 0, canva.width, canva.height);
+
+            ctx.beginPath();
+            ctx.arc(pointer.current.x , pointer.current.y,6, 0, Math.PI * 2);
+            ctx.fillStyle = "red";
+            ctx.fill();
+            requestAnimationFrame(draw)
+            }
+
+
+        const resize = ()=>{
+            canva.height = window.innerHeight
+            canva.width = window.innerWidth
+
+        }
+        resize();
+        draw();
+        
+        window.addEventListener('resize',resize);
+        
+        
+        return ()=>{ 
+            window.removeEventListener('resize',resize);
+            window.removeEventListener('mousemove',updatepointer);
+            
+            
+        }
+        
+        
 
 
     },[])
-    
+        console.log(pointer)
 
     
 
 
     return (
-        <canvas ref={canvaref} className="flex justify-center items-center h-screen bg-black"/>
+        <canvas ref={canvaref} className="flex justify-center items-center h-screen w-screen bg-gray-700  cursor-none "/>
     )
 
 }
