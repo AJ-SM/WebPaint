6a064db4e112923e01a8de8c4685d0a06f7eafb9
AJ-SM <anujsidam1108@gmail.com>
2025-10-09 22:13:33 +0530

    minor adjust

--- wpaint/app/board/page.tsx
+++ wpaint/app/board/page.tsx
@@ -25,10 +25,47 @@ export default function Board(){
             ctx.fillStyle = "black";
             ctx.fillRect(0, 0, canva.width, canva.height);
 
-            ctx.beginPath();
-            ctx.arc(pointer.current.x , pointer.current.y,6, 0, Math.PI * 2);
-            ctx.fillStyle = "red";
-            ctx.fill();
+
+            // Next Code here 
+
+            // Line Here 
+            function line(){
+         
+            const startPos = { x: 0, y: 0 };
+
+            const handleDown = () => {
+                startPos.x = pointer.current.x;
+                startPos.y = pointer.current.y;
+            };
+
+            const handleUp = () => {
+                const endPos = { x: pointer.current.x, y: pointer.current.y };
+                ctx?.beginPath();
+                ctx?.moveTo(startPos.x, startPos.y);
+                ctx?.lineTo(endPos.x, endPos.y);
+                ctx!.lineWidth = 10;
+                ctx!.strokeStyle = "white";
+                ctx!.stroke();
+            };
+
+            window.addEventListener("mousedown", handleDown);
+            window.addEventListener("mouseup", handleUp);
+
+            window.removeEventListener("mousedown", handleDown);
+            window.removeEventListener("mouseup", handleUp);
+            
+            
+
+
+
+            }
+
+
+
+
+
+            line();
+
             requestAnimationFrame(draw)
             }
 
@@ -61,7 +98,7 @@ export default function Board(){
 
 
     return (
-        <canvas ref={canvaref} className="flex justify-center items-center h-screen w-screen bg-gray-700  cursor-none "/>
+        <canvas ref={canvaref} className="flex justify-center items-center h-screen w-screen bg-gray-700"/>
     )
 
 }
