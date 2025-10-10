70dad87b0c9021917f280944eea307a86116766b
AJ-SM <anujsidam1108@gmail.com>
2025-10-10 22:19:40 +0530

    added line logic

--- wpaint/app/board/page.tsx
+++ wpaint/app/board/page.tsx
@@ -5,12 +5,11 @@ import React, { useEffect, useRef, useState } from "react"
 
 export default function Board(){
     const canvaref = useRef<HTMLCanvasElement>(null)
+    const isPainting = useRef<boolean>(false)
     const pointer = useRef({x:0,y:0})
-
     function updatepointer(e:MouseEvent){
         pointer.current = {x:e.clientX,y:e.clientY}
     }
-
 
 
     useEffect(()=>{
@@ -20,50 +19,39 @@ export default function Board(){
 
         const ctx = canva.getContext('2d');
         if(!ctx) return;
+    
+        
+      function startPostion(){
+          isPainting.current=true
+          
+        }
+            function EndPostion(){
+            isPainting.current = !isPainting.current
+            ctx?.beginPath()
+            }
 
             const draw = ()=>{
-            ctx.fillStyle = "black";
-            ctx.fillRect(0, 0, canva.width, canva.height);
+            // ctx.fillStyle = "black";
+            // ctx.fillRect(0, 0, canva.width, canva.height);
 
 
             // Next Code here 
 
             // Line Here 
-            function line(){
-         
-            const startPos = { x: 0, y: 0 };
+function line() {
+  
 
-            const handleDown = () => {
-                startPos.x = pointer.current.x;
-                startPos.y = pointer.current.y;
-            };
+    if(isPainting.current){
+    if(!ctx) return;
+    ctx.lineWidth=10;
+    ctx.lineCap='round'
+    ctx.lineTo(pointer.current.x,pointer.current.y)
+    ctx.strokeStyle = "white"
+    ctx.stroke()
+  }
 
-            const handleUp = () => {
-                const endPos = { x: pointer.current.x, y: pointer.current.y };
-                ctx?.beginPath();
-                ctx?.moveTo(startPos.x, startPos.y);
-                ctx?.lineTo(endPos.x, endPos.y);
-                ctx!.lineWidth = 10;
-                ctx!.strokeStyle = "white";
-                ctx!.stroke();
-            };
-
-            window.addEventListener("mousedown", handleDown);
-            window.addEventListener("mouseup", handleUp);
-
-            window.removeEventListener("mousedown", handleDown);
-            window.removeEventListener("mouseup", handleUp);
-            
-            
-
-
-
-            }
-
-
-
-
-
+  
+}
             line();
 
             requestAnimationFrame(draw)
@@ -79,26 +67,27 @@ export default function Board(){
         draw();
         
         window.addEventListener('resize',resize);
-        
+        window.addEventListener("mousedown", startPostion);
+        window.addEventListener("mouseup", EndPostion);
         
         return ()=>{ 
             window.removeEventListener('resize',resize);
-            window.removeEventListener('mousemove',updatepointer);
-            
-            
+            window.removeEventListener('mousemove',updatepointer); 
+            window.removeEventListener("mousedown", startPostion);
+            window.removeEventListener("mouseup", EndPostion);
         }
         
         
 
 
     },[])
-        console.log(pointer)
+        
 
     
 
 
     return (
-        <canvas ref={canvaref} className="flex justify-center items-center h-screen w-screen bg-gray-700"/>
+        <canvas ref={canvaref} className="flex justify-center items-center h-screen w-screen bg-black"/>
     )
 
 }
