d1cedbbca6bcf262c5bcc03b131c10fd23f03b06
AJ-SM <anujsidam1108@gmail.com>
2025-10-13 22:31:54 +0530

    added workings

--- wpaint/app/board/page.tsx
+++ wpaint/app/board/page.tsx
@@ -9,10 +9,12 @@ export default function Board(){
     const pointer = useRef({x:0,y:0})
     const startval = useRef({x:0,y:0})
     const endval = useRef({x:0,y:0})
+    const pencil = false;
 
     function updatepointer(e:MouseEvent){
         pointer.current = {x:e.clientX,y:e.clientY}
     }
+    
 
 
     useEffect(()=>{
@@ -23,11 +25,12 @@ export default function Board(){
         const ctx = canva.getContext('2d');
         if(!ctx) return;
     
-        
+      
+
       function startPostion(){
           isPainting.current=true
           startval.current = pointer.current
-          console.log(startval.current)
+        
           
         }
             function EndPostion(){
@@ -46,7 +49,6 @@ export default function Board(){
 
             // Line Here 
             function line() {
-            
 
                 if(isPainting.current){
                 if(!ctx) return;
@@ -55,7 +57,11 @@ export default function Board(){
                 ctx.lineTo(pointer.current.x,pointer.current.y)
                 ctx.strokeStyle = "white"
                 ctx.stroke()
+            
+
+
             }
+
 
             
             }
@@ -70,7 +76,7 @@ export default function Board(){
             }
 
 
-            line();
+            line()
             // Rect();
 
             requestAnimationFrame(draw)
@@ -89,6 +95,9 @@ export default function Board(){
         window.addEventListener('resize',resize);
         window.addEventListener("mousedown", startPostion);
         window.addEventListener("mouseup", EndPostion);
+
+
+
         
         return ()=>{ 
             window.removeEventListener('resize',resize);
@@ -101,23 +110,33 @@ export default function Board(){
 
 
     },[])
+
+
+                function clearCanvas() {
+            const canva = canvaref.current;
+            if (!canva) return;
+            const ctx = canva.getContext("2d");
+            if (!ctx) return;
+            ctx.clearRect(0, 0, canva.width, canva.height);
+            }
         
 
     
 
 
     return (
+        
         <>
         <div className=" absolute mt-5 flex justify-center items-center w-screen  h-9 bg-black" >
              <div className=" p-5 mt-1 flex gap-10 items-center absolute bg-white h-9 text-black w-115 rounded-md "> 
 
-            <div className=" cursor-pointer hover:bg-slate-200 h-9 flex justify-center items-center w-9 p-2 rounded-2xl">
+            <div className=" cursor-pointer hover:bg-slate-200 h-9 flex justify-center items-center w-9 p-2 rounded-2xl" onClick={()=>{return}}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                 </svg>
 
             </div>
-            <div className=" cursor-pointer hover:bg-slate-200 h-9 flex justify-center items-center w-9 p-2 rounded-2xl">
+            <div className=" cursor-pointer hover:bg-slate-200 h-9 flex justify-center items-center w-9 p-2 rounded-2xl" onClick={clearCanvas}>
 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
@@ -136,6 +155,8 @@ export default function Board(){
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
                     </svg>
+
+                    
 
 
             </div>
